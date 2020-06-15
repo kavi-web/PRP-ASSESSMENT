92fc8f49874a24838042006c750810f74bab5237
kavi-web <59790793+kavi-web@users.noreply.github.com>
2020-06-15 22:47:35 +0530

    Add files via upload

---
+++ John.java
@@ -0,0 +1,10 @@
+
+public class John {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String s1=args[0];
+        System.out.println("Welcome"+" "+s1);
+	}
+
+}
