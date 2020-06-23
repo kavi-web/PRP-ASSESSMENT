b584190e04b72418977762b402d9bee48ba13c1c
kavi-web <59790793+kavi-web@users.noreply.github.com>
2020-06-23 23:12:49 +0530

    Add files via upload

---
+++ Language basics/Sample.java
@@ -0,0 +1,12 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1=args[0];
+		String s2=args[1];
+		System.out.println(s1+" "+"Technologies"+" "+s2+".");
+	}
+
+}
---
+++ Language basics/Sum.java
@@ -0,0 +1,14 @@
+
+
+public class Sum {
+	public static void main(String args[]) 
+	{
+		int val1=Integer.parseInt(args[0]);
+		int val2=Integer.parseInt(args[1]);
+		int sum=val1+val2;
+		System.out.println("The sum of " + val1 +" and "+ val2 + " is " +sum);
+	}
+
+
+
+}
